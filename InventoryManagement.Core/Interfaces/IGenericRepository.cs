5c5c9a35769c805f8d69c3393ab03af12b19bbe2
IbrahimSajib <ibrahimsajib.bd@gmail.com>
2025-12-21 01:04:16 +0600

    Implement Generic Repository

---
+++ InventoryManagement.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+using InventoryManagement.Core.Common;
+
+namespace InventoryManagement.Core.Interfaces;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    // Query methods
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+
+    // LINQ Query Syntax support
+    IQueryable<T> GetQueryable();
+    IQueryable<T> GetQueryable(Expression<Func<T, bool>> predicate);
+
+    // Command methods
+    Task<T> AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+    void Update(T entity);
+    void UpdateRange(IEnumerable<T> entities);
+    void Delete(T entity);
+    void DeleteRange(IEnumerable<T> entities);
+
+    // Soft delete
+    void SoftDelete(T entity);
+    void SoftDeleteRange(IEnumerable<T> entities);
+}
---
+++ InventoryManagement.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,121 @@
+﻿using System.Linq.Expressions;
+using InventoryManagement.Core.Common;
+using InventoryManagement.Core.Interfaces;
+using InventoryManagement.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Infrastructure.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    protected readonly ApplicationDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    // Query methods
+    public virtual async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public virtual async Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.FirstOrDefaultAsync(predicate);
+    }
+
+    public virtual async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.AnyAsync(predicate);
+    }
+
+    public virtual async Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null)
+    {
+        return predicate == null ? await _dbSet.CountAsync() : await _dbSet.CountAsync(predicate);
+    }
+
+    // LINQ Query Syntax support
+    public virtual IQueryable<T> GetQueryable()
+    {
+        return _dbSet.AsQueryable();
+    }
+
+    public virtual IQueryable<T> GetQueryable(Expression<Func<T, bool>> predicate)
+    {
+        return _dbSet.Where(predicate).AsQueryable();
+    }
+
+    // Command methods
+    public virtual async Task<T> AddAsync(T entity)
+    {
+        entity.CreatedAt = DateTime.UtcNow;
+        await _dbSet.AddAsync(entity);
+        return entity;
+    }
+
+    public virtual async Task AddRangeAsync(IEnumerable<T> entities)
+    {
+        foreach (var entity in entities)
+        {
+            entity.CreatedAt = DateTime.UtcNow;
+        }
+        await _dbSet.AddRangeAsync(entities);
+    }
+
+    public virtual void Update(T entity)
+    {
+        entity.UpdatedAt = DateTime.UtcNow;
+        _dbSet.Update(entity);
+    }
+
+    public virtual void UpdateRange(IEnumerable<T> entities)
+    {
+        foreach (var entity in entities)
+        {
+            entity.UpdatedAt = DateTime.UtcNow;
+        }
+        _dbSet.UpdateRange(entities);
+    }
+
+    public virtual void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public virtual void DeleteRange(IEnumerable<T> entities)
+    {
+        _dbSet.RemoveRange(entities);
+    }
+
+    // Soft delete
+    public virtual void SoftDelete(T entity)
+    {
+        entity.IsDeleted = true;
+        entity.UpdatedAt = DateTime.UtcNow;
+        _dbSet.Update(entity);
+    }
+
+    public virtual void SoftDeleteRange(IEnumerable<T> entities)
+    {
+        foreach (var entity in entities)
+        {
+            entity.IsDeleted = true;
+            entity.UpdatedAt = DateTime.UtcNow;
+        }
+        _dbSet.UpdateRange(entities);
+    }
+}
